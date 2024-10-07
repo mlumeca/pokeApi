75abf9011232a266e3ca272b298c787878f9b05f
Candi Alcantarilla <alcantarilla.cacan24@triana.salesianos.edu>
2024-10-07 17:41:33 +0200

    plantilla de mostrar listado de pokemons utilizando la api

--- pokeApi/js/listaPokemon.js
+++ pokeApi/js/listaPokemon.js
@@ -1,22 +1,21 @@
 $(document).ready(function () {
-<<<<<<< HEAD
-    getPokemonListV2();
+  getPokemonListV2();
 
 
 
-    function getPokemonListV2() {
+  function getPokemonListV2() {
 
-        $("#listaPokemon").html("<img src='loading.gif' />");
-        $.ajax({
-            url: "https://pokeapi.co/api/v2/pokemon",
-            method: "GET",
-        }).done(function (pokemon) {
+    $("#listaPokemon").html("<img src='loading.gif' />");
+    $.ajax({
+      url: "https://pokeapi.co/api/v2/pokemon",
+      method: "GET",
+    }).done(function (pokemon) {
 
-            var listadoPomemon = pokemon.results;
-            listadoPomemon.forEach(function (pokemon) {
-                var pokemonId = pokemon.url.split("/")[6];
+      var listadoPomemon = pokemon.results;
+      listadoPomemon.forEach(function (pokemon) {
+        var pokemonId = pokemon.url.split("/")[6];
 
-                var template = `
+        var template = `
                 <div class="d-flex justify-content-around col-3">
 
                     <div class="pokemon-card">
@@ -34,38 +33,12 @@ $(document).ready(function () {
 
                 </div>
                 `;
-                $('#listaPokemon').append(template);
-            });
+        $('#listaPokemon').append(template);
+      });
 
-        });
-    }
+    });
+  }
 });
 
 
 
-=======
-    getPokemonList();
-  
-    $(document).on("click", "#btn-get-data", function () {
-      getPokemonList();
-    });
-  
-    function getPokemonList() {
-      $("#data-content").html("<img src='loading.gif' />");
-      $.ajax({
-        url: "https://pokeapi.co/api/v2/pokemon",
-        method: "GET",
-      }).done(function (resp) {
-        setTimeout(function () {
-          $("#data-content").html("");
-          var listadoPomemon = resp.results;
-          listadoPomemon.forEach(function (pokemon) {
-            var pokemonId = pokemon.url.split("/")[6];
-            var template = `<p><a href="detail.html?pid=${pokemonId}"><h1 class="pokemon" pokemonid="1">${pokemon.name}</h1><img src="https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/${pokemonId}.png"</a></p>`;
-            $("#data-content").append(template);
-          });
-        }, 1000);
-      });
-    }
-  });
->>>>>>> e47854b024dc38d4172fc8aa42f81f72768d7992
